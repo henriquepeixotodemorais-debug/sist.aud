9ff131b0408f97c5c7c6e055cfcc2a252b9d0396
henriquepeixotodemorais-debug <henrique.peixoto.demorais@gmail.com>
2026-01-26 10:57:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -230,19 +230,12 @@ if df.empty:
     st.warning("Nenhuma base encontrada. Entre com a chave 'sisbase' e faça o upload do CSV para iniciar.")
     st.stop()
 
-# converte coluna "data e horário" para datetime com dayfirst=True
-# df["data e horário"] = pd.to_datetime(df["data e horário"], dayfirst=True, errors="coerce")
-# df["dia"] = df["data e horário"].dt.strftime("%d/%m/%y")
-
 # df = df.sort_values(["dia", "sala de audiência", "data e horário"])
-# df = df.sort_values([ "data e horário","dia", "sala de audiência"])
-from datetime import datetime
-
-df["data e horário"] = pd.to_datetime(df["data e horário"], dayfirst=True, errors="coerce")
-df["dia"] = df["data e horário"].dt.strftime("%d/%m/%y")
 
 # ordena globalmente por data/hora e sala
-df = df.sort_values(["data e horário", "sala de audiência", "número do processo relacionado"], ascending=True).reset_index(drop=True)
+# df = df.sort_values(["sala de audiência"], ascending=True).reset_index(drop=True)
+df = df.sort_values(["dia", "sala de audiência", "data e horário"])
+# df = df.sort_values([ "dia", "data e horário","sala de audiência"])
 
 
 
